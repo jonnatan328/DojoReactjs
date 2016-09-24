5c9cf856e2627aeff59418096f612c29cb9458c6
Carlos <carloskikea@gmail.com>
2016-09-24 09:36:05 -0500

    Coco proyecto

--- actions/MotelActions.js
+++
@@ -1,7 +0,0 @@
-import Reflux from 'reflux';
-
-let MotelActions = Reflux.createActions([
-  'fetchList'
-]);
-
-export default MotelActions;
--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MotelActions from './actions/MotelActions';
-import MotelGrid from './components/Motelgrid.jsx';
+import App from './components/App.jsx';
 
-// Cada 5 segundos: recargar!
-setInterval(function() {
-    MotelActions.fetchList(); 
-}, 5000);
-
-ReactDOM.render(<MotelGrid />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
